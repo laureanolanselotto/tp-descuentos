310b17f9eb0928acef872d4fe809aa88995e2bc6
laureanolanselotto <laureanolanselotto1998@gmail.com>
2025-07-18 20:09:16 -0300

    la el crud persona funciona con mongodb. tendria que en el fron probar si guarda y por ultimo hacer el crud de beneficio el cual es el mismo que el de persona

--- src/personas/personas.repository.ts
+++ src/personas/personas.repository.ts
@@ -2,6 +2,7 @@
 import { persona } from "./personas.entity.js";
 import { Repository } from "../shared/repository.js";
 import {db} from "../shared/db/conn.js";
+import { ObjectId } from "mongodb";
 const personasArray =  [
     new persona(
         'lauerano',
@@ -20,24 +21,25 @@ export class PersonasRepository implements Repository <persona> {
         return await personas.find().toArray();
     }
     public async findOne(nombre: {id: string}): Promise<persona | undefined> {
-        return await personasArray.find((persona)=> persona.id === nombre.id);
+        const _id = new ObjectId(nombre.id);
+        return (await personas.findOne({_id})||undefined);// Esto busca en la base de datos usando el id como ObjectId
+        // Si no se encuentra, devuelve undefined
+
+       // return await personasArray.find((persona)=> persona.id === nombre.id);
     }
     public async add(nombre: persona): Promise<persona | undefined> {
-        await personasArray.push(nombre);
-        return await nombre;
+        (await personas.insertOne(nombre)).insertedId;
+        return  nombre;
     }
     public async update(nombre : persona): Promise<persona | undefined> {
-        const index = await personasArray.findIndex((persona) => persona.id === nombre.id);
-        if (index !== -1) {
-            personasArray[index] = {...personasArray[index], ...nombre};  
-        }
-        return await personasArray[index];
-    }
+        const {id,...personasInput} = nombre; // Desestructuramos para obtener el id y el resto de los campos
+        const _id = new ObjectId(nombre.id);
+        return (await personas.findOneAndUpdate({_id}, {$set: personasInput},{returnDocument: 'after'}))||undefined;// no poner llaves alrededor de nombre//
+    }// findOneAndUpdate busca un documento y lo actualiza, devolviendo el documento actualizado
+    // Si no se encuentra, devuelve undefined
     public async delete(nombre: {id: string}): Promise<persona | undefined> {
-        const personaIdx = await personasArray.findIndex((persona) => persona.id === nombre.id)
-    if (personaIdx !== -1) {
-    const deletePersonas  = personasArray[personaIdx];
-    personasArray.splice(personaIdx, 1); // Elimina la persona del array , el uno es para indicar que se elimina un elemento
-    return await deletePersonas;
-    }}
+        const _id = new ObjectId(nombre.id);
+        return (await personas.findOneAndDelete({ _id })) || undefined;// Esto busca en la base de datos usando el id como ObjectId
+        // Si no se encuentra, devuelve undefined
+    }
 }
